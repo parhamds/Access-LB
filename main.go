5dad31bddbee2eb2c04bb70752c87769da37e71b
parhamds <parham.doustkani@gmail.com>
2023-09-16 08:56:40 +0200

    v0.0.23

--- main.go
+++ main.go
@@ -151,12 +151,12 @@ func execAddRule(gwip, ueip string) error {
 	if err != nil {
 		return err
 	}
-	m32Rule := fmt.Sprint("\"0x38&0xffff0000=0x", hexIP, "\"")
+	m32Rule := fmt.Sprint("\"0x38&0xffffffff=0x", hexIP, "\"")
 	cmd := exec.Command("iptables", "-t", "mangle", "-A", "PREROUTING", "-d", "192.168.252.3", "-p", "udp", "--dport", "2152", "-m", "u32", "--u32", m32Rule, "-j", "MARK", "--set-mark", mark)
-	err = cmd.Run()
+	combinedOutput, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Errorf("Error executing command: %v", cmd.String())
-		return err
+		fmt.Printf("Error executing command: %v\nCombined Output: %s", cmd.String(), combinedOutput)
+
 	}
 	log.Traceln("iptables rule applied successfully for ip : ", ueip)
 	return nil
